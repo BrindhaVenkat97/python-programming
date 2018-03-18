8c3b362081d9d103f1d4838da785ebe46b30a20b
BrindhaVenkat97 <37336219+BrindhaVenkat97@users.noreply.github.com>
2018-03-18 05:53:00 +0000

    Create find largest number.py

---
+++ beginner level/find largest number.py
@@ -0,0 +1,9 @@
+a=input("enter the first num: ")
+b=input("enter the second num: ")
+c=input("enter the third num: ")
+if(a>b and a>c):
+	print("a")
+elif(b>c and b>a):
+	print("b")
+else:
+	print("c")
