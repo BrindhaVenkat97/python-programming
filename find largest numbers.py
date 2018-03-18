83928dccac9ebc3eb7dd04e66e3828898de4dc07
BrindhaVenkat97 <37336219+BrindhaVenkat97@users.noreply.github.com>
2018-03-18 05:52:35 +0000

    Delete find largest numbers.py

--- find largest numbers.py
+++
@@ -1,9 +0,0 @@
-a=input("enter the first num: ")
-b=input("enter the second num: ")
-c=input("enter the third num: ")
-if(a>b and a>c):
-	print("a")
-elif(b>c and b>a):
-	print("b")
-else:
-	print("c")
